45b3ac9f234684184d4416c101bd7e20317a19c8
Renee Vandervelde <Renee@ReneeVandervelde.com>
2024-05-26 11:02:37 -0500

    Update to kotlin 2.0

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -64,7 +64,6 @@ kotlin {
         val commonTest by getting {
             dependencies {
                 implementation(libs.kotlin.test.core)
-                implementation(libs.kotlin.test.junit)
                 implementation(libs.coroutines.test)
             }
         }
